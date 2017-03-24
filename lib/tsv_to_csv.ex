f83f0adb910956207648b62d4f7b95b30cbf2fcc
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-03-24 16:29:04 +0530

    init

---
+++ lib/tsv_to_csv.ex
@@ -0,0 +1,18 @@
+defmodule TsvToCsv do
+  @moduledoc """
+  Documentation for TsvToCsv.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TsvToCsv.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tsv_to_csv_test.exs
@@ -0,0 +1,8 @@
+defmodule TsvToCsvTest do
+  use ExUnit.Case
+  doctest TsvToCsv
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
